97600887fd1d4004ecf9def8badc0097c9b0b53c
k4nkan <kanta.y.0126@gmail.com>
2025-02-15 02:12:17 +0900

    #5 ヘッダーの作成、ディレクトリ構造の変更

---
+++ front-end/src/components/header.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="bg-[#DD9004] text-white h-16 fixed top-0 left-0 w-full flex items-center">
+      <div className="max-w-6xl w-full mx-auto px-6 flex items-center justify-center gap-8">
+        {/* ロゴ */}
+        <div className="flex gap-2">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={40}
+            height={40}
+            className="h-8 w-auto"
+          />
+
+          <Link href="/" className="text-2xl font-bold">
+            Dishcode
+          </Link>
+        </div>
+
+        <div className="flex-1 max-w-md relative">
+          <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+            <Image src="/search.svg" alt="search icon" width={20} height={20} />
+          </div>
+
+          <input
+            type="text"
+            placeholder="料理名・食材でレシピを検索"
+            className="w-full h-10 px-10 pl-10 rounded-full text-black"
+          />
+        </div>
+
+        <div className="bg-white rounded-lg p-1">
+          <Image
+            src="/menu.svg"
+            alt="menu"
+            width={40}
+            height={40}
+            className="h-8 w-auto cursor-pointer"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
