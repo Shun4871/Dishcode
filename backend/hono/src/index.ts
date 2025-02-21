775abdf4489a219ec8ccc6b05c2bcbbcefe3be4e
aya <116853251+yamamoto-ayano@users.noreply.github.com>
2025-02-22 04:42:34 +0900

    クラークログインしたら、データベースに行くようになったよ

--- backend/hono/src/index.ts
+++ backend/hono/src/index.ts
@@ -7,24 +7,80 @@ import { WebhookEvent } from '@clerk/backend'
 import { Webhook } from 'svix'
 
 import { z } from 'zod'
-import { zValidate } from '@hono/zod-validate'
+// import { zValidate } from '@hono/zod-validate'
 
 const app = new Hono()
 
 const prisma = new PrismaClient();
 
-.post("/webhook/clerk",async(c)=>{
-    const SIGNING_SECRET = process.env.
+const db = prisma
 
-    if(!SIGNING_SECRET){
-        return c.status(500).send("Internal Server Error")
+app.use('*', clerkMiddleware())
+app.get('/', (c) => {
+    const auth = getAuth(c)
+
+    if (!auth?.userId) {
+        return c.json({
+            message: 'You are not logged in.'
+        })
     }
 
-)
+    return c.json({
+        message: 'You are logged in!',
+        userId: auth.userId
+    })
+})
+
+    .post("/webhook/clerk", async (c) => {
+        const SIGNING_SECRET = process.env.SIGNING_SECRET
+
+        if (!SIGNING_SECRET) {
+            throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+        }
+        const wh = new Webhook(SIGNING_SECRET)
+        const svix_id = c.req.header('svix-id')
+        const svix_timestamp = c.req.header('svix-timestamp')
+        const svix_signature = c.req.header('svix-signature')
+        if (!svix_id || !svix_timestamp || !svix_signature) {
+            return c.json({ message: 'Error: Missing Svix headers' }, {
+                status: 400,
+            })
+        }
+        const payload = await c.req.json()
+        const body = JSON.stringify(payload)
+
+        let evt: WebhookEvent
+
+        try {
+            evt = wh.verify(body, {
+                'svix-id': svix_id,
+                'svix-timestamp': svix_timestamp,
+                'svix-signature': svix_signature,
+            }) as WebhookEvent
+        } catch (err) {
+            console.error('Error: Could not verify webhook:', err)
+            return c.json({ message: 'Error: Verification error' }, {
+                status: 400,
+            })
+        }
+
+        if (evt.type === 'user.created') {
+            await prisma.user.create({
+                data: {
+                    clerk_id: evt.data.id,
+                }
+            })
+            return c.json({ message: 'User created successfully' }, {
+                status: 200
+            })
+        }
+        return c.json({ message: 'Error: Invalid event type' }, {
+            status: 400
+        })
+    })
 
 
-
-export default{
+export default {
     port: 8000,
     fetch: app.fetch
 } 
