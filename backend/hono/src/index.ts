7b51694858cdc50c1f032643e17ca642163e004b
aya <116853251+yamamoto-ayano@users.noreply.github.com>
2025-02-22 00:52:54 +0900

    必要そうなの入れた

--- backend/hono/src/index.ts
+++ backend/hono/src/index.ts
@@ -1,8 +1,30 @@
 
 import { Hono } from 'hono'
 
+import { Prisma, PrismaClient } from "@prisma/client";
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { WebhookEvent } from '@clerk/backend'
+import { Webhook } from 'svix'
+
+import { z } from 'zod'
+import { zValidate } from '@hono/zod-validate'
+
+const app = new Hono()
+
+const prisma = new PrismaClient();
+
+.post("/webhook/clerk",async(c)=>{
+    const SIGNING_SECRET = process.env.
+
+    if(!SIGNING_SECRET){
+        return c.status(500).send("Internal Server Error")
+    }
+
+)
 
 
 
-export default app
+export default{
+    port: 8000,
+    fetch: app.fetch
+} 
