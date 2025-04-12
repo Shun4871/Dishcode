c45d9158ce3880878ce6edff33ba8bd603921f6b
ShunnosukeTsuge <shuntsugework@gmail.com>
2025-04-13 01:19:12 +0900

    変更

--- front-end/src/components/ClerkWrapper.tsx
+++ front-end/src/components/ClerkWrapper.tsx
@@ -1,16 +1,9 @@
-// src/components/ui/ClerkWrapper.tsx
+// src/components/ClerkWrapper.tsx
 
 "use client";
 
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function ClerkWrapper({ children }: { children: React.ReactNode }) {
-  const publishableKey = process.env.pk_test_cGlja2VkLWtvZGlhay0xOS5jbGVyay5hY2NvdW50cy5kZXYk;
-
-  if (!publishableKey) {
-    console.error("ClerkのpublishableKeyが設定されていません。");
-    return <div>Clerkの設定エラー</div>;
-  }
-
-  return <ClerkProvider publishableKey={publishableKey}>{children}</ClerkProvider>;
+  return <ClerkProvider>{children}</ClerkProvider>;
 }
