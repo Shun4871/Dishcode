d4bc7787b76aa06eae5f01c2d69b6bee35525fc4
ShunnosukeTsuge <shuntsugework@gmail.com>
2025-04-18 01:10:44 +0900

    設定画面

--- front-end/src/app/(dashboard)/setting/page.tsx
+++ front-end/src/app/(dashboard)/setting/page.tsx
@@ -1,8 +1,18 @@
-export default function SettingPage() {
-    return (
-      <div>
-        <h1>Settings</h1>
-        <p>ここに設定ページの内容を書く</p>
-      </div>
-    );
-  }
+// psetting/page.tsx
+import type { NextPage } from 'next';
+import Allergy from '@/app/(dashboard)/setting/_components/Allergy';
+import { Button } from '@/components/ui/button';
+import { Flex } from '@/components/ui/flex';
+const AllergiesPage: NextPage = () => (
+  <div style={{ maxWidth: '600px', margin: '2rem auto', padding: '0 1rem' }}>
+    <h1 className ="font-black pt-8 pb-10 text-2xl">アレルギー項目</h1>
+    <Allergy />
+    <Flex>
+    <Button>
+      保存
+    </Button>
+    </Flex>
+  </div>
+);
+
+export default AllergiesPage;  
