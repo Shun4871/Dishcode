ed504a3d7f555ddf4525b28d63c0ef1321bd7d89
aya <116853251+yamamoto-ayano@users.noreply.github.com>
2025-04-18 16:01:41 +0900

    もしBrouseruseが無理だったらchatGPTが補う

--- backend/agent/result_formatter.py
+++ backend/agent/result_formatter.py
@@ -9,6 +9,7 @@ def result_formatter(text: str):
     prompt = f"""与えるテキストから3つのurlを摘出してください。
     https://www.yahoo.co.jp/で検索してください、
     フォーマットは以下のようにして、余分な改行やスペースは入れないようにしてください。
+    もし、urlが3つ見つからなかった場合は、似たようなレシピサイトのurlを3つ出力してください。
     {{"url1":"https://sample.url.1","url2":"https://sample.url.2","url3":"https://sample.url.3"}}
     抽出元のテキスト: {text}
     """
