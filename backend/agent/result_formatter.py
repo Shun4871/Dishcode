7c1a0306a182b6507129eee3968de97a42daf7b0
k4nkan <kanta.y.0126@gmail.com>
2025-04-19 09:25:15 +0900

    [fix] result formatter adjust

--- backend/agent/result_formatter.py
+++ backend/agent/result_formatter.py
@@ -7,8 +7,7 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def result_formatter(text: str):
     prompt = f"""与えるテキストから3つのurlを摘出してください。
-    https://www.yahoo.co.jp/で検索してください、
-    フォーマットは以下のようにして、余分な改行やスペースは入れないようにしてください。
+    フォーマットは以下のようにして、余分な改行やスペース、記号を入れないようにしてください。
     もし、urlが3つ見つからなかった場合は、似たようなレシピサイトのurlを3つ出力してください。
     {{"url1":"https://sample.url.1","url2":"https://sample.url.2","url3":"https://sample.url.3"}}
     抽出元のテキスト: {text}
